d2a59ad2f80dd1bbeb81dfddd2ae72858ed6f75d
Amit Kumar <65532249+amitlamba12@users.noreply.github.com>
2022-06-22 22:42:17 +0530

    Update sendGridKey.js

--- config/sendGridKey.js
+++ config/sendGridKey.js
@@ -1,6 +1,6 @@
 const SEND_GRID_API_KEY =
   // amit account -
-  "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"
+  "SendGridAPI"
   module.exports = {
     SEND_GRID_API_KEY,
   };
